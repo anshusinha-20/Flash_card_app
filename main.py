7032c0979765cc1b70d174572ba4d8e79a665e5f
Anshu Sinha <anshujuly2@gmail.com>
2023-04-11 18:52:13 +0530

    Add a code to generate and display new words on the flash card

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 """imported every class from the tkinter module"""
 from tkinter import *
 
+"""imported pandas module"""
+import pandas
+
+"""imported random module"""
+import random
+
 
 # ---------------------------- CONSTANTS ------------------------------- #
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_LANG = ("Ariel", 40, "italic")
 FONT_WORD = ("Ariel", 60, "bold")
+
+
+# ---------------------------- GENERATING RANDOM WORDS ------------------------------- #
+"""variable to store the csv data"""
+data = pandas.read_csv("data/french_words.csv")
+
+"""converted to list of dictionaries"""
+dataList = data.to_dict(orient="records")
+
+"""variable to store the length of the dataList"""
+dataListLength = len(dataList)
+
+"""function to display random french words"""
+def randomFrench():
+    word = dataList[random.randint(0, dataListLength - 1)]["French"]
+    canvas.itemconfig(wordText, text=word)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -31,12 +53,12 @@ canvas.grid(row=1, column=1, columnspan=2)
 
 """created the buttonWrong object"""
 wrongImage = PhotoImage(file="images/wrong.png")
-buttonWrong = Button(image=wrongImage, bd=0, highlightthickness=0)
+buttonWrong = Button(image=wrongImage, bd=0, highlightthickness=0, command=randomFrench)
 buttonWrong.grid(row=2, column=1)
 
 """created the buttonWrong object"""
 rightImage = PhotoImage(file="images/right.png")
-buttonWrong = Button(image=rightImage, bd=0, highlightthickness=0)
+buttonWrong = Button(image=rightImage, bd=0, highlightthickness=0, command=randomFrench)
 buttonWrong.grid(row=2, column=2)
 
 
