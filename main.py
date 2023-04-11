66cbbf6144eeb9130dab420f93b8f0681b4f8d86
Anshu Sinha <anshujuly2@gmail.com>
2023-04-11 20:35:24 +0530

    Add a code to flip the present card

--- main.py
+++ main.py
@@ -12,6 +12,36 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_LANG = ("Ariel", 40, "italic")
 FONT_WORD = ("Ariel", 60, "bold")
+timer = None
+randomNum = None
+
+
+# ---------------------------- CHANGING FLASH CARDS ------------------------------- #
+"""function to change to front card"""
+def changeToFront():
+    canvas.itemconfig(cardImage, image=cardFront)
+    canvas.itemconfig(langText, text="French", fill="black")
+    canvas.itemconfig(wordText, text=randomFrench(), fill="black")
+
+    global timer
+    timer = window.after(3000, changeToBack)
+
+
+
+"""function to change to back card"""
+def changeToBack():
+    canvas.itemconfig(cardImage, image=cardBack)
+    canvas.itemconfig(langText, text="English", fill="white")
+    canvas.itemconfig(wordText, text=randomEnglish(), fill="white")
+
+    window.after_cancel(timer)
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+"""function to start the timer"""
+def startTimer():
+    global timer
+    timer = window.after(3000, changeToBack)
 
 
 # ---------------------------- GENERATING RANDOM WORDS ------------------------------- #
@@ -24,10 +54,19 @@ dataList = data.to_dict(orient="records")
 """variable to store the length of the dataList"""
 dataListLength = len(dataList)
 
-"""function to display random french words"""
+"""function to return random french word"""
 def randomFrench():
-    word = dataList[random.randint(0, dataListLength - 1)]["French"]
-    canvas.itemconfig(wordText, text=word)
+    global randomNum
+    """variable to store random number"""
+    randomNum = random.randint(0, dataListLength - 1)
+
+    wordFrench = dataList[randomNum]["French"]
+    return wordFrench
+
+"""function to return random english word"""
+def randomEnglish():
+    wordEnglish = dataList[randomNum]["English"]
+    return wordEnglish
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -44,23 +83,26 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 """created the canvas object"""
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 cardFront = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=cardFront)
+cardBack = PhotoImage(file="images/card_back.png")
+cardImage = canvas.create_image(400, 263, image=cardFront)
 langText = canvas.create_text(400, 150, text="French", font=FONT_LANG, fill="black")
-wordText = canvas.create_text(400, 263, text="trouve", font=FONT_WORD, fill="black")
+wordText = canvas.create_text(400, 263, text=randomFrench(), font=FONT_WORD, fill="black")
 canvas.grid(row=1, column=1, columnspan=2)
 
 # button
 
 """created the buttonWrong object"""
 wrongImage = PhotoImage(file="images/wrong.png")
-buttonWrong = Button(image=wrongImage, bd=0, highlightthickness=0, command=randomFrench)
+buttonWrong = Button(image=wrongImage, bd=0, highlightthickness=0, command=changeToFront)
 buttonWrong.grid(row=2, column=1)
 
 """created the buttonWrong object"""
 rightImage = PhotoImage(file="images/right.png")
-buttonWrong = Button(image=rightImage, bd=0, highlightthickness=0, command=randomFrench)
+buttonWrong = Button(image=rightImage, bd=0, highlightthickness=0, command=changeToFront)
 buttonWrong.grid(row=2, column=2)
 
+"""initial start timer"""
+startTimer()
 
 """loop to keep the window open"""
 window.mainloop()
